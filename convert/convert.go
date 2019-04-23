aaa008b7f416837eb4e684878b8f00b8ed3ba629
xiagoo <xiagoo.github.com>
2019-04-23 16:44:42 +0800

    add conv

--- convert/convert.go
+++ convert/convert.go
@@ -1,9 +1,13 @@
 package convert
 
 import (
+	"fmt"
 	"reflect"
+	"strings"
+	"unicode"
 )
 
+//ConvStruct2Map convert struct to map[string]interface{}
 func ConvStruct2Map(source interface{}) map[string]interface{} {
 	target := make(map[string]interface{})
 	st := reflect.TypeOf(source)
@@ -14,8 +18,74 @@ func ConvStruct2Map(source interface{}) map[string]interface{} {
 	}
 	if st.Kind() == reflect.Struct {
 		for i := 0; i < sv.Type().NumField(); i++ {
-			target[sv.Type().Field(i).Tag.Get("json")] = sv.Field(i).Interface()
+			tag := sv.Type().Field(i).Tag.Get("json")
+			if tag == "-" {
+				continue
+			}
+
+			name, opts := parseTag(tag)
+			if !isValidTag(name) {
+				continue
+			}
+
+			if strings.Contains(opts, "omitempty") && isEmptyValue(sv.Field(i)) {
+				continue
+			}
+			target[name] = sv.Field(i).Interface()
 		}
+	}
+	return target
+}
+
+func parseTag(tag string) (string, string) {
+	if idx := strings.Index(tag, ","); idx != -1 {
+		return tag[:idx], tag[idx+1:]
+	}
+	return tag, ""
+}
+
+func isValidTag(s string) bool {
+	if s == "" {
+		return false
+	}
+	for _, c := range s {
+		switch {
+		case strings.ContainsRune("!#$%&()*+-./:<=>?@[]^_{|}~ ", c):
+		case !unicode.IsLetter(c) && !unicode.IsDigit(c):
+			return false
+		}
+	}
+	return true
+}
+
+func isEmptyValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	}
+	return false
+}
+
+//ConvIntf2Str ...
+func ConvIntf2Str(source interface{}) string {
+	return fmt.Sprintf("%v", source)
+}
+
+//ConvMapIntf2MapStr ...
+func ConvMapIntf2MapStr(source map[string]interface{}) map[string]string {
+	target := make(map[string]string)
+	for k, v := range source {
+		target[k] = ConvIntf2Str(v)
 	}
 	return target
 }
--- convert/convert_test.go
+++ convert/convert_test.go
@@ -6,11 +6,23 @@ import (
 
 func Test_ConvStruct2Map(t *testing.T) {
 	source := struct {
-		Name string `json:"name"`
+		Name string `json:"name,omitempty"`
 		Age  int    `json:"age"`
 	}{
 		Name: "lily",
 		Age:  30,
 	}
 	t.Log(ConvStruct2Map(&source))
+}
+
+func Test_ConvIntf2Str(t *testing.T) {
+	t.Log(ConvIntf2Str("test"))
+	t.Log(ConvIntf2Str(1))
+}
+
+func Test_ConvMapIntf2MapStr(t *testing.T) {
+	t.Log(ConvMapIntf2MapStr(map[string]interface{}{
+		"name": "lily",
+		"age":  20,
+	}))
 }
