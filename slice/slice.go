6a49a78525db5bb5fa47e3cd87668c8614020131
xiagoo <xiagoo.github.com>
2020-10-14 14:09:29 +0800

    add duplicate

--- slice/slice.go
+++ slice/slice.go
@@ -74,3 +74,14 @@ func ArrayMerge(slice interface{}, others ...interface{}) (interface{}, error) {
 	}
 	return rv, nil
 }
+
+func RemoveDuplicate(a []string) []string {
+	ret := []string{}
+	for i := 0; i < len(a); i++ {
+		if (i > 0 && a[i-1] == a[i]) || len(a[i]) == 0 {
+			continue
+		}
+		ret = append(ret, a[i])
+	}
+	return ret
+}
